4a74bbd6f898c9515722baa7ccb299b7b134baad
JohnMushattWPI <33138767+JohnMushattWPI@users.noreply.github.com>
2017-11-08 18:34:53 -0500

    Updated CacheTest

--- LRUCache/src/CacheTest.java
+++ LRUCache/src/CacheTest.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.util.HashMap;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,34 +20,53 @@ public class CacheTest {
 	
 	
 	}
-	//Test order when calling get on a cache
+	//Test order when calling get on a cache, use data provider  method getNumMisses()
 	@Test
 	public void testLRU()
 	{
-		DataProvider<Integer,String> provider = new Provider(); // Need to instantiate an actual DataProvider
+		HashMap<Integer,String> testStorage = new HashMap<Integer, String>(5);
+		DataProvider<Integer,String> provider = new Provider(testStorage); // Need to instantiate an actual DataProvider
 		Cache<Integer,String> cache = new LRUCache<Integer,String>(provider, 5);
 		
 		cache.get(1);
+		cache.get(2);
+		cache.get(3);
+		cache.get(4);
+		cache.get(5);
+	}
+	
+	@Test
+	public void testInt()
+	{
 		
 	}
+	@Test
+	public void testString()
+	{
+		
+	}
+	@Test
+	public void testObject()
+	{
+		
+	}
+	   
 	//Test based on System.time
 	@Test
 	public void testTime()
 	{
+		has
 		final long startTime = System.currentTimeMillis();
 		DataProvider<Integer,String> provider = new Provider(); // Need to instantiate an actual DataProvider
 		Cache<Integer,String> cache = new LRUCache<Integer,String>(provider, 5);
-		for(int i = 0; i < cache.getCapacity(); i++)
-		{
-			cache.get(i);
-		}
+		//Fill cache with stuff
 		
 		
 			
 		final long endTime = System.currentTimeMillis();
 		
 		final long totalTime = endTime-startTime;
-		final long  tester= 100000000;
+		final long tester= 100000000;
 		assertEquals(totalTime < tester, true);
 		
 	}
