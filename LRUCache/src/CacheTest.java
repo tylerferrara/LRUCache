894987ca99e69b01046a904caecefbea213920fa
JohnMushattWPI <33138767+JohnMushattWPI@users.noreply.github.com>
2017-11-08 20:14:25 -0500

    Updated CacheTest

--- LRUCache/src/CacheTest.java
+++ LRUCache/src/CacheTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.junit.Before;
@@ -29,48 +30,84 @@ public class CacheTest {
 		Cache<Integer,String> cache = new LRUCache<Integer,String>(provider, 5);
 		int misses = provider.getNumMisses();
 		System.out.println(misses);
-		assertEquals(provider.getNumMisses(), provider.getNumMisses()==0);
-		assertEquals(cache.get(1), provider.getNumMisses()==misses+1);
-		System.out.println(misses);
+		for(int i =0; i < testStorage.size();i++)
+		{
+			testStorage.put(i, Integer.toString(i));
+		}
+		
+		assertEquals(provider.getNumMisses(), 0);
+		//assertEquals(, provider.getNumMisses()==misses+1);
+		//System.out.println(misses);
+		cache.get(1);
+		assertEquals(provider.getNumMisses(), misses+1);
 		cache.get(2);
 		cache.get(3);
 		cache.get(4);
 		cache.get(5);
+		cache.get(1);
+		assertEquals(provider.getNumMisses(),misses+1);
 	}
 	
+	
+	
 	@Test
-	public void testInt()
+	public void testOther()
 	{
+		HashMap<String,String> testStorage = new HashMap<String, String>(5);
+		Provider<String,String> provider = new Provider(testStorage); // Need to instantiate an actual DataProvider
+		Cache<String,String> cache = new LRUCache<String,String>(provider, 5);
+		int misses = provider.getNumMisses();
+		System.out.println(misses);
+		for(int i =0; i < testStorage.size();i++)
+		{
+			testStorage.put("" + i, "" + i);
+		}
 		
-	}
-	@Test
-	public void testString()
-	{
-		
-	}
-	@Test
-	public void testObject()
-	{
-		
+		assertEquals(provider.getNumMisses(), 0);
+		//assertEquals(, provider.getNumMisses()==misses+1);
+		//System.out.println(misses);
+		cache.get("1");
+		assertEquals(provider.getNumMisses(), misses+1);
+		cache.get("2");
+		cache.get("3");
+		cache.get("4");
+		cache.get("5");
+		cache.get("1");
+		assertEquals(provider.getNumMisses(),misses+1);	
 	}
 	   
 	//Test based on System.time
 	@Test
 	public void testTime()
 	{
-		HashMap<Integer,String> testStorage = new HashMap<Integer, String>(5);
-		Provider<Integer,String> provider = new Provider(testStorage); // Need to instantiate an actual DataProvider
-		Cache<Integer,String> cache = new LRUCache<Integer,String>(provider, 5);
-		final long startTime = System.currentTimeMillis();
+		HashMap<Integer,Integer> testStorage = new HashMap<Integer, Integer>(10000000);
+		for(int i = 0; i <testStorage.size();i++)
+		{
+			testStorage.put(i, i);
+		}
+		
+		Provider<Integer,Integer> provider = new Provider(testStorage); // Need to instantiate an actual DataProvider
+		Cache<Integer,Integer> cache = new LRUCache<Integer,Integer>(provider, 5);
+		
 		//Fill cache with stuff
+		ArrayList<LRUCache<Integer,Integer>> cacheArray = new ArrayList();
+		for(int i=0; i < 100;i++)
+		{
+			cacheArray.add(new LRUCache<Integer,Integer>(provider, i));
+		}
 		
-		
-			
+		final long[] cacheStartTimes = new long[cacheArray.size()];
+		final long[] cacheEndTimes = new long[cacheArray.size()];
+		for(int i=0; i< cacheArray.size();i++)
+		{
+			cacheStartTimes[i] = System.currentTimeMillis();
+			//for(int j =0; )
+			cacheEndTimes[i] = System.currentTimeMillis();
+		}
 		final long endTime = System.currentTimeMillis();
 		
-		final long totalTime = endTime-startTime;
-		final long tester= 100000000;
-		assertEquals(totalTime < tester, true);
+		
+		
 		
 	}
 	
